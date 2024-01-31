e9964174eda87ff828281c0f81d1d4a9722efe37
Костянтин <71712209+DocQuanty@users.noreply.github.com>
2024-01-31 20:13:00 +0200

    Restructured grid table, started create adaptive for grid

--- src/components/UserTable/UserTable.tsx
+++ src/components/UserTable/UserTable.tsx
@@ -26,28 +26,30 @@ const UserTable: FC<UserTableProps> = ({ data, active }) => {
     console.log('[data]', data)
 
     return (
-        <div className={s.parent}>
-            <div className={s.info_block}>#</div>
-            <div className={s.info_block}>Artist</div>
-            <div className={s.info_block}>Change</div>
-            <div className={s.info_block}>NFTs Sold</div>
-            <div className={s.info_block}>Volume</div>
-            {data.map((user) => (
-                <Fragment key={user.id}>
-                    <div className={s.sequence}>{user.id}</div>
-                    <div className={s.artist}>
-                        <span className={s.img}>
-                            <img src={user.img.src} alt="alfredo" />
-                        </span>
-                        <div className={s.name} >{user.name}</div>
-                    </div>
 
+        <div className={s.parent}>
+            <div className={s.header}>
+                <div className={s.nested}>#</div>
+                <div className={s.nested}>Artist</div>
+                <div className={s.nested}>Change</div>
+                <div className={s.nested}>NFTs Sold</div>
+                <div className={s.nested}>NFTs Volume</div>
+            </div>
+            {data.map((user) => (
+                <div key={user.id} className={s.content_table}>
+                    <div className="box">{user.id}</div>
+                    <div className={s.img_block}>
+                        <img src={user.img.src} alt="table_img" />
+                        <span className={s.userName}>
+                            {user.name}
+                        </span>
+                    </div>
                     <div className={s.change}>+1.41%</div>
                     <div className={s.sold}>
-                        {active === "ThisWeek" ? user.sold + 100 : user.sold}
-                    </div>
-                    <div className={s.volume}>{active === "ThisWeek" ? user.volume + 100 : user.volume} ETH</div>
-                </Fragment>
+                        {active === "ThisWeek" ? user.sold + 100 : user.sold}</div>
+                    <div className={s.volume}>
+                        {active === "ThisWeek" ? user.volume + 100 : user.volume} ETH</div>
+                </div>
             ))}
         </div>
     )
